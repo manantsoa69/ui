bb1962db64d9472f667bde45c0dea8c296861365
manantsoa69 <138488477+manantsoa69@users.noreply.github.com>
2023-07-26 22:54:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,28 +6,54 @@ const http = require('http');
 const socketIO = require('socket.io');
 const axios = require('axios');
 const mongoose = require('mongoose');
-const { saveNumberAndFBID, getStoredNumbers } = require('./mongodb');
+const Redis = require('ioredis');
+
+const { getStoredNumbers } = require('./redis');
 
 require('dotenv').config();
-const subUrl = process.env.SUB_URL || 'https://server0.adaptable.app';
+
+mongoose.connect(process.env.MONGODB_URI || '', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const queryResultSchema = new mongoose.Schema({
+  number: String,
+  fbid: String,
+  receivedate: Date,
+});
+
+const QueryResultModel = mongoose.model('QueryResult', queryResultSchema);
+
+const redisUrl = process.env.REDIS_URL || '';
+const redisClient = new Redis(redisUrl);
+
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => {
-  console.log('Connected to MongoDB');
-}).catch(error => {
-  console.error('Failed to connect to MongoDB', error);
-});
 
 app.get('/query', async (req, res) => {
   try {
     const numberToQuery = req.query.number || '';
 
     const items = await getStoredNumbers(numberToQuery);
+
+    // Step 3: Save the queried data to MongoDB
+    await Promise.all(
+      items.map(async (item) => {
+        const { number, fbid, receivedate } = item;
+        const queryResult = new QueryResultModel({ number, fbid, receivedate });
+        await queryResult.save();
+      })
+    );
+
+    // Step 4: Delete the data from Redis after successfully saving to MongoDB
+    await Promise.all(
+      items.map(async (item) => {
+        const key = item.number; // Assuming the "number" field is used as the Redis key
+        await redisClient.del(key);
+      })
+    );
 
     res.json({ numberToQuery, items });
   } catch (error) {
@@ -36,33 +62,6 @@ app.get('/query', async (req, res) => {
   }
 });
 
-app.post('/api/numbers', async (req, res) => {
-  try {
-    const { number, fbid } = req.body;
-
-    console.log('Received number:', number, 'FBID:', fbid);
-
-    await saveNumberAndFBID(number, fbid);
-
-    res.status(200).json({ message: 'Number and FBID received' });
-
-    io.emit('newNumber', { number, fbid });
-  } catch (error) {
-    console.error('Error:', error);
-    res.status(500).json({ error: 'An error occurred' });
-  }
-});
-
-app.get('/api/numbers', async (req, res) => {
-  try {
-    const items = await getStoredNumbers();
-
-    res.json(items);
-  } catch (error) {
-    console.error('Error:', error);
-    res.status(500).json({ error: 'An error occurred' });
-  }
-});
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
@@ -73,10 +72,10 @@ app.post('/subscribe', async (req, res) => {
 
     const payload = {
       fbid,
-      subscriptionStatus
+      subscriptionStatus,
     };
 
-    const response = await axios.post(`${subUrl}/subscribe`, new URLSearchParams(payload));
+    const response = await axios.post('https://usersub.onrender.com/subscribe', new URLSearchParams(payload));
 
     const responseData = response.data;
 
@@ -87,32 +86,108 @@ app.post('/subscribe', async (req, res) => {
   }
 });
 
-app.post('/send_message', async (req, res) => {
+app.post('/send_message', async (req, res, next) => {
   try {
     const { fbid } = req.body;
 
-    if (!fbid) {
-      res.status(400).json({ error: 'Missing required parameter: fbid' });
-      return;
+    console.log('Received message request:', { fbid });
+
+    const success = await sendMessage(fbid);
+
+    if (success) {
+      console.log('Message sent successfully.');
+      res.status(200).json({ message: 'Message sent successfully.' });
+    } else {
+      console.error('Failed to send message.');
+      res.status(500).json({ message: 'Failed to send message.' });
     }
-
-    const payload = {
-      fbid
-    };
-
-    const response = await axios.post('https://server0-ikandraenligne.b4a.run/send_message', new URLSearchParams(payload));
-
-    const responseData = response.data;
-    res.json(responseData);
   } catch (error) {
     console.error('Error sending message:', error);
-    res.status(500).json({ error: 'An error occurred' });
+    next(error);
   }
 });
+
+async function sendMessage(fbid) {
+  const url = 'https://usersub.onrender.com/send_message';
+  const data = { fbid };
+
+  try {
+    const response = await axios.post(url, data);
+    return true;
+  } catch (error) {
+    console.error('Error sending the message:', error.message);
+    return false;
+  }
+}
+
 
 app.get('/', (req, res) => {
   res.render('form', { numberToQuery: '' });
 });
+async function sendMessagesToNumbers() {
+  try {
+    let cursor = '0';
+    const oneDayInMilliseconds = 24 * 60 * 60 * 1000; // 1 day in milliseconds
+
+    // Get the current date in milliseconds
+    const currentDateInMs = Date.now();
+
+    // Loop until SCAN returns '0' (end of iteration)
+    do {
+      // Use the SCAN command to get a batch of keys (numbers)
+      const [newCursor, keys] = await redisClient.scan(cursor, 'MATCH', '*');
+
+      // Update the cursor for the next iteration
+      cursor = newCursor;
+
+      // Send messages to each number (key) in the current batch
+      for (const key of keys) {
+        try {
+          const numberData = await redisClient.hgetall(key);
+          const { fbid, receivedate } = numberData;
+
+          if (!fbid) {
+            console.error(`Missing fbid for key: ${key}`);
+            continue;
+          }
+
+          // Calculate the time difference between receive date and current date
+          const receiveDateInMs = Date.parse(receivedate);
+          const timeDifferenceInMs = currentDateInMs - receiveDateInMs;
+
+          // If the time difference is greater than 1 day, send the post request
+          if (timeDifferenceInMs >= oneDayInMilliseconds) {
+            const success = await sendMessage(fbid);
+            if (success) {
+              console.log(`Message sent successfully to fbid: ${fbid}`);
+              // Delete the data related to fbid in Redis after sending the message successfully
+              await redisClient.hdel(key, 'number', 'receivedate', 'fbid'); // Delete all fields in the hash
+            } else {
+              console.error(`Failed to send message to fbid: ${fbid}`);
+            }
+          } else {
+            console.log(`Message for fbid: ${fbid} not sent. Receive date within 1 day.`);
+          }
+        } catch (error) {
+          console.error(`Error processing key: ${key}`, error);
+        }
+      }
+    } while (cursor !== '0'); // Loop until the end of iteration
+
+    // Reschedule the function to run again in one day
+    setTimeout(sendMessagesToNumbers, oneDayInMilliseconds);
+  } catch (error) {
+    console.error('Error sending messages:', error);
+  }
+}
+
+// Start sending messages to all numbers every day for testing
+sendMessagesToNumbers();
+
+app.get('/', (req, res) => {
+  res.render('form', { numberToQuery: '' });
+});
+
 
 const port = 3002;
 const server = http.createServer(app);
